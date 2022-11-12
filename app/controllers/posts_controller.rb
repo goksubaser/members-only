a2d8d11cf3eed8c4016de208e8c944ffacf8dd39
goksubaser <goksubaser@gmail.com>
2022-11-12 16:56:40 +0000

    before_action line added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user, only: [:new, :update]
 
+
+
+  private
+  def authenticate_user
+    #sum authantication
+  end
 end
